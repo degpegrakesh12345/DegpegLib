baa40b737b9c152d89e268b5689d10820a71b7c5
Rakesh Kumar <rakeshkumark@wekancode.comNew meetingMy meetingsHangoutsIntroduction of iOS lead Rakesh KumarInboxRajesh TripathyHi All, Please welcome Rakesh Kumar who has joined Religare team as lead for iOS. He would be managing the day to day tasks and activities for iOS InstaBiz / FaJun 21, 2021, 9:17 AM (2 days ago)Avneesh Minocha avneesh.minocha@kelltontech.comJun 21, 2021, 10:54 AM (2 days ago)to Rajesh, me, Anoop, Indra, Vikas, Neelu, Chejarla, amod.sengar, tarun.kaushik1, Kripa, Inderjit, RishipalHi Rakesh>
2022-03-09 08:37:05 +0530

    Added test cases

--- DegpegLibTests/DegpegLibTests.swift
+++ DegpegLibTests/DegpegLibTests.swift
@@ -4,31 +4,25 @@
 //
 //  Created by Apple on 3/9/22.
 //  Copyright © 2022 Apple. All rights reserved.
-//
 
 import XCTest
+import DegpegLib
 @testable import DegpegLib
 
 class DegpegLibTests: XCTestCase {
+    
+    var swiftyLib: SwiftyLib!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
 
 }
---
+++ SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  DegpegLib.swift
+//  DegpegLib
+//
+//  Created by Apple on 3/9/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+public final class SwiftyLib {
+
+    let name = "DegpegLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
