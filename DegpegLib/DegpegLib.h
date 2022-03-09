290809b14571acf41abad852686a2310eabd5881
Rakesh Kumar <rakeshkumark@wekancode.comNew meetingMy meetingsHangoutsIntroduction of iOS lead Rakesh KumarInboxRajesh TripathyHi All, Please welcome Rakesh Kumar who has joined Religare team as lead for iOS. He would be managing the day to day tasks and activities for iOS InstaBiz / FaJun 21, 2021, 9:17 AM (2 days ago)Avneesh Minocha avneesh.minocha@kelltontech.comJun 21, 2021, 10:54 AM (2 days ago)to Rajesh, me, Anoop, Indra, Vikas, Neelu, Chejarla, amod.sengar, tarun.kaushik1, Kripa, Inderjit, RishipalHi Rakesh>
2022-03-09 08:04:57 +0530

    Initial project setup

---
+++ DegpegLib/DegpegLib.h
@@ -0,0 +1,19 @@
+//
+//  DegpegLib.h
+//  DegpegLib
+//
+//  Created by Apple on 3/9/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DegpegLib.
+FOUNDATION_EXPORT double DegpegLibVersionNumber;
+
+//! Project version string for DegpegLib.
+FOUNDATION_EXPORT const unsigned char DegpegLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DegpegLib/PublicHeader.h>
+
+
---
+++ DegpegLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DegpegLibExamples
+//
+//  Created by Apple on 3/9/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
